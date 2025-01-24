8f7d06cd77fd6606266a1b1886160d4c3df99ee9
moparisthebest <admin@moparisthebest.com>
2025-01-24 02:06:19 -0500

    bare minimum changes to make siderakb/ergosnm/rev2 compile and work with latest vial

--- keyboards/siderakb/ergosnm/keymaps/default/keymap.c
+++ keyboards/siderakb/ergosnm/keymaps/default/keymap.c
@@ -3,7 +3,7 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     // clang-format off
 [0] = LAYOUT(
-  KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
+  QK_GESC, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_BSLS,
   KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_BSPC,
   KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,
   KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                               KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT, 
@@ -133,6 +133,10 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 void led_set_user(uint8_t usb_led) {
+    // USB_LED_NUM_LOCK and friends were removed in:
+    // https://github.com/qmk/qmk_firmware/pull/21424/files
+    // but this doesn't do anythnig anyway so just comment out
+    /*
     if (usb_led & (1 << USB_LED_NUM_LOCK)) {
     } else {
     }
@@ -152,4 +156,5 @@ void led_set_user(uint8_t usb_led) {
     if (usb_led & (1 << USB_LED_KANA)) {
     } else {
     }
+    */
 }
